915559b4578751027a74a7e834fccb4975845a1b
Furkan Karakas <furkan.karakas@epfl.ch>
2024-01-01 15:43:55 +0100

    navbar update

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -5,72 +5,37 @@ export default function Navbar() {
     <nav className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
-              <a href="/src/assets/furkan.png" target="_blank">
-                <img
-                  className="h-10 w-auto"
-                  src="/src/assets/furkan.png"
-                  alt="Furkan Karakaş"
-                />
-              </a>
-            </div>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                <CustomLink
-                  to="/"
-                  className="text-white rounded-md px-3 py-2 text-sm font-medium"
-                  aria-current="page"
-                >
-                  Home
-                </CustomLink>
+          <a href="/src/assets/furkan.png" target="_blank">
+            <img
+              className="h-10 w-auto"
+              src="/src/assets/furkan.png"
+              alt="Furkan Karakaş"
+            />
+          </a>
 
-                <CustomLink
-                  to="/about"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >
-                  About
-                </CustomLink>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <CustomLink
+              to="/"
+              className="text-white rounded-md px-3 py-2 text-sm font-medium"
+              aria-current="page"
+            >
+              Home
+            </CustomLink>
 
-                <CustomLink
-                  to="/blog"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >
-                  Blog Posts
-                </CustomLink>
-              </div>
-            </div>
+            <CustomLink
+              to="/about"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+            >
+              About
+            </CustomLink>
+
+            <CustomLink
+              to="/blog"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+            >
+              Blog Posts
+            </CustomLink>
           </div>
-        </div>
-      </div>
-
-      <div className="sm:hidden" id="mobile-menu">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          <a
-            href="#"
-            className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-            aria-current="page"
-          >
-            Dashboard
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Team
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Projects
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Calendar
-          </a>
         </div>
       </div>
     </nav>
