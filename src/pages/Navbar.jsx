e8fbbe26908c8b8f6ac6c3faa4e72765b9f2fb96
Furkan Karakas <furkan.karakas@epfl.ch>
2024-01-01 14:39:21 +0100

    navbar

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -2,14 +2,77 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="nav">
-      <Link to="/" className="site-title">
-        Furkan Karakaş | Personal Website
-      </Link>
-      <ul>
-        <CustomLink to="/about">About</CustomLink>
-        <CustomLink to="/blog">Blog Posts</CustomLink>
-      </ul>
+    <nav className="bg-gray-800">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-shrink-0 items-center">
+              <a href="/src/assets/furkan.png" target="_blank">
+                <img
+                  className="h-10 w-auto"
+                  src="/src/assets/furkan.png"
+                  alt="Furkan Karakaş"
+                />
+              </a>
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                <CustomLink
+                  to="/"
+                  className="text-white rounded-md px-3 py-2 text-sm font-medium"
+                  aria-current="page"
+                >
+                  Home
+                </CustomLink>
+
+                <CustomLink
+                  to="/about"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                >
+                  About
+                </CustomLink>
+
+                <CustomLink
+                  to="/blog"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                >
+                  Blog Posts
+                </CustomLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="sm:hidden" id="mobile-menu">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          <a
+            href="#"
+            className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+            aria-current="page"
+          >
+            Dashboard
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+          >
+            Team
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+          >
+            Projects
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+          >
+            Calendar
+          </a>
+        </div>
+      </div>
     </nav>
   );
 }
@@ -19,10 +82,12 @@ function CustomLink({ to, children, ...props }) {
   const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
   return (
-    <li className={isActive ? "active" : ""}>
-      <Link to={to} {...props}>
-        {children}
-      </Link>
-    </li>
+    <Link
+      to={to}
+      {...props}
+      className={`${props.className} ${isActive ? "bg-gray-600" : ""}`}
+    >
+      {children}
+    </Link>
   );
 }
