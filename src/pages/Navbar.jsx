23df4b7e2066ccae54c083191228d7a5f7ea1a85
Furkan Karakas <furkan.karakas@epfl.ch>
2024-01-02 03:46:33 +0100

    static assets

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,16 +1,13 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import imgUrl from "/src/assets/furkan.png";
 
 export default function Navbar() {
   return (
     <nav className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <a href="/src/assets/furkan.png" target="_blank">
-            <img
-              className="h-10 w-auto"
-              src="/src/assets/furkan.png"
-              alt="Furkan Karakaş"
-            />
+          <a href={imgUrl} target="_blank" rel="noreferrer">
+            <img className="h-10 w-auto" src={imgUrl} alt="Furkan Karakaş" />
           </a>
 
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
