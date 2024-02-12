a762a8dda26f12e151325c57d19abf1f9e9e54a3
Furkan Karakas <f.karakas@zzag.ch>
2024-02-12 17:19:45 +0100

    not found page added

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,15 @@
 import { Container, Typography } from "@mui/material";
+import AnimatedPage from "../components/AnimatedPage.jsx";
 
 export default function NotFound() {
   return (
-    <Container>
-      <Typography variant={"h1"}>404</Typography>
-      <Typography variant={"body1"}>The requested URL wasn't found!</Typography>
-    </Container>
+    <AnimatedPage>
+      <Container>
+        <Typography variant={"h1"}>404</Typography>
+        <Typography variant={"body1"}>
+          The requested URL wasn't found!
+        </Typography>
+      </Container>
+    </AnimatedPage>
   );
 }
